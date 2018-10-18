ff058dcd8b3cd5e542838c5a3251046013bc8346
JetBlackForest <k.aguilar28@aol.com>
2018-10-18 12:53:31 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kevin Aguilar on 10/18/18.
+//  Copyright © 2018 Kevin Aguilar. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
